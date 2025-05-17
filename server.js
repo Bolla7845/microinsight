ee3ed62048a7193fb77ad71a1b2ee5c5368f684f
Bolla7845 <antropotix@libero.it>
2025-05-17 23:10:13 +0200

    Update server.js
    fuiki

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: { 
-    secure: process.env.NODE_ENV === 'production',
+    secure: false, // Temporaneamente false per debug
     maxAge: 1000 * 60 * 60 * 24 // 1 giorno
   }
 }));
